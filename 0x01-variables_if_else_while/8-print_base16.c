371ab06cea032e46bf3f60a72e78f6f147cbbcc2
Abdelwahab-Hamada <abdelwahab.hamada@gmail.com>
2023-03-17 12:29:07 -0700

    fix 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,20 +7,13 @@
  */
 int main(void)
 {
-	unsigned char a = '0';
-	int i;
+	int n = 48;
 
-	for (i = 0; i < 10; i++)
-	{
-		putchar(a);
-		a++;
-	}
-	a = '1';
-	for (i = 0; i < 6; i++)
-	{
-		putchar('0' + a);
-		a++;
-	}
+	for (n ; n < 58 ; n++)
+		putchar(n);
+	n = 97;
+	for (n ; n < 103 ; n++)
+		putchar(n);
 	putchar('\n');
 	return (0);
 }
