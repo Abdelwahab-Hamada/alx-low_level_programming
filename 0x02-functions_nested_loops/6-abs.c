48d4cbc0da2485eb2a6f79af61a2c979aa6aa05a
Abdelwahab-Hamada <abdelwahab.hamada@gmail.com>
2023-03-21 05:06:36 -0700

    6 & 7

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include"main.h"
+/**
+ * _abs - main
+ *
+ * @n: input
+ *
+ * Return: absolute number
+ *
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		return (n * -1);
+	return (n);
+}
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include"main.h"
+/**
+ * print_last_digit - main
+ *
+ * @n: input
+ *
+ * Return: last digit
+ *
+ */
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
