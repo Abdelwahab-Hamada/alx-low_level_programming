d2a57da8d43a73e2e4c8334988e30707f8e5f0ca
Abdelwahab-Hamada <abdelwahab.hamada@gmail.com>
2023-03-23 05:29:17 -0700

    10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include"main.h"
+/**
+ * print_triangle
+ * @size: input
+ */
+void print_triangle(int size)
+{
+	int row;
+	int space;
+	int hash;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	} else
+	{
+		for (row = 1 ; row <= size ; row++)
+		{
+			for(space = size - 1 ; space > 0 ; space--)
+			{
+				_putchar(' ');
+			}
+			for (hash = 0 ; hash < row ; hash++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
