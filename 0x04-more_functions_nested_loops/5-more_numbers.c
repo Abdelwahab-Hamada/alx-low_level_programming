6854e96e9fd6f14c8ffad19d7849a08e4bb4c56e
Abdelwahab-Hamada <abdelwahab.hamada@gmail.com>
2023-03-23 04:02:32 -0700

    5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,11 +9,14 @@ void more_numbers(void)
 
 	for (counter = 1 ; counter <= 10 ; counter++)
 	{
-		for (n = '0' ; n <= '14' ; n++)
+		for (n = 0 ; n <= 14 ; n++)
 		{
-			_putchar(n);
+			if (n > 9)
+			{
+				_putchar(n / 10 + '0');
+			}
+			_putchar(n % 10 + '0');
 		}
 		_putchar('\n');
 	}
-	_putchar('\n');
 }
