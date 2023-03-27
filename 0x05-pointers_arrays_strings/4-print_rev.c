e074e44c7c8f1711ee0db3424b7f0b468e895343
Abdelwahab-Hamada <abdelwahab.hamada@gmail.com>
2023-03-26 23:21:05 -0700

    4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,4 +1,5 @@
 #include"main.h"
+#include "2-strlen.c"
 /**
  * print_rev - mian
  * @s: input
@@ -7,7 +8,7 @@ void print_rev(char *s)
 {
 	int index;
 	
-	for (index = _strlen(s) ; index >= 0 ; index--)
+	for (index = _strlen(s) - 1 ; index >= 0 ; index--)
 		_putchar(s[index]);
 	_putchar('\n');
 }
